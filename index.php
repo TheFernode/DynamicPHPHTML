db8810c643b4636c1318245336af7017cbcd39e8
StarLand <smirkogm@gmail.com>
2015-04-29 17:45:13 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once "setup.php";
+require_once "functions.php";
+
+$page = isset($_GET['page']) ? $_GET['page'] : "default";
+$pc = getPage($page);
+?>
+<html lang="de">
+<head>
+<link rel="stylesheet" type="text/css" href="design/style.css">
+<title><?php echo $website_title; ?></title>	
+</head>
+<body>
+	<?php include "subpages/header.php"; ?>
+	<div class="fieldmain">
+	<table class="width100">
+	<tr>
+	<td class="width15" align="center" style="vertical-align: top;" rowspan="2"><?php require "subpages/sidebar.php"; ?></td>
+	<td class="width85"><?php print $pc; ?></td>
+	</tr>
+	<tr>
+	<td class="width90">
+	</div>
+	</table>
+	</div>
+</body>
+</html>
