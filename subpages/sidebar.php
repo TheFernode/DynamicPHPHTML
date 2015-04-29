9b4c4222ee324fcd14fb7a92a099150adc9cc6e4
StarLand <smirkogm@gmail.com>
2015-04-29 17:50:15 +0200

    Create sidebar.php

---
+++ subpages/sidebar.php
@@ -0,0 +1,6 @@
+<div id="sidebar">
+  <div style="padding-top:8px;">Navigation</div>
+  <hr>
+  <a href="?page=default"><button>Home</button></a><br>
+  <a href="?page=sample"><button>Sample</button></a>
+</div>
