96e575b3e012f80c105eac1767b7a4a7f826348e
StarLand <smirkogm@gmail.com>
2015-04-29 17:49:38 +0200

    Create header.php

---
+++ subpages/header.php
@@ -0,0 +1,14 @@
+<div class="field">
+<table class="width100">
+	<tr>
+		<td class="width60">
+			<a href="./"><img src="http://www.starland-lair.ml/img/logo_phphtml.png"></img></a>
+		</td>
+		<td>
+			<form action="#search" method="post">
+				<input class="width80" name="search"></input>
+				<input value="Suchen" type="submit"></input>
+			</form>
+		</td>
+	</tr>
+</table>
