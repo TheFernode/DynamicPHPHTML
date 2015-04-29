c6c670869dff7b7bb7d7d5f74de549fbb57ba61a
StarLand <smirkogm@gmail.com>
2015-04-29 17:46:20 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+require_once "setup.php";
+
+function getPage($pagename)
+{
+	global $website_pages;
+	$path = "$website_pages/$pagename.php";
+	
+	if(file_exists($path))
+	{
+		return openPage($path);
+	}
+	else
+	{
+		return openPage("$website_pages/default.php");
+	}
+	
+}
+
+function openPage($pageurl)
+{
+	$fh = fopen($pageurl, "r");
+	$fc = fread($fh,  filesize($pageurl));
+	//fclose($fh);
+	return $fc;
+}
+?>
