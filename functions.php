ad30affdb5ecbfe393333a3cf7539203b836a67e
Fernode <smirkogm@gmail.com>
2018-11-26 13:23:39 +0100

    Shortings...

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-require_once "setup.php";
+require_once("setup.php");
 
 function getPage($pagename)
 {
@@ -7,21 +7,15 @@ function getPage($pagename)
 	$path = "$website_pages/$pagename.php";
 	
 	if(file_exists($path))
-	{
 		return openPage($path);
-	}
 	else
-	{
 		return openPage("$website_pages/default.php");
-	}
-	
 }
 
 function openPage($pageurl)
 {
 	$fh = fopen($pageurl, "r");
-	$fc = fread($fh,  filesize($pageurl));
-	//fclose($fh);
+	$fc = fread($fh, filesize($pageurl));
 	return $fc;
 }
 ?>
